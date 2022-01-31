037a9bf390fba59361a24abce16e58d0a8f0af0b
James Dibble <11923585+dibble-james@users.noreply.github.com>
2022-01-31 22:20:45 +0000

    Add docs (#3)
    * Start to build a nicer docs site
    
    * More docs improvements
    
    Co-authored-by: James Dibble <dibble-james@github.com>

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Demo;
 using Demo.Data;
+using BlazorStrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazorStrap();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
