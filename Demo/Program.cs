66ad72a0971c57a1f61f5852d9cea8861e030b2f
James Dibble <dibble-james@github.com>
2022-02-01 20:39:03 +0000

    Update docs

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Demo;
-using Demo.Data;
 using BlazorStrap;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,7 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazorStrap();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
