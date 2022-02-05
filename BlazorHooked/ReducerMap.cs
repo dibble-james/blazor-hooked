6fd02ee240f50849d80e635868834146e0548698
James Dibble <dibble-james@github.com>
2022-02-05 14:17:22 +0000

    First attempt at a connector

---
+++ BlazorHooked/ReducerMap.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorHooked;
+
+public delegate Action DispatchAction(object action);
+
+public delegate T ReducerExecutor<T>(T state);
+
+public delegate T ReducerCombiner<T, TSubState>(T state, ReducerExecutor<TSubState> reducer);
+
+public static class CombinedReducer
+{
+    public static Reducer<T> CombineReducer<T, TSubState>(
+        this Reducer<T> parentReducer, ReducerCombiner<T, TSubState> map, Reducer<TSubState> reducer) => (state, action) =>
+        map(parentReducer(state, action), subState => reducer(subState, action));
+}
