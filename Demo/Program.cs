89f6097f9023d2f294ca68f57fa1a21189287f3d
James Dibble <11923585+dibble-james@users.noreply.github.com>
2022-02-08 22:05:50 +0000

    Create a global store with connect constructs (#8)
    * First attempt at a connector
    
    * Fix connector
    
    * Switch to hxbootstrap
    
    * Fixup the docs
    
    Co-authored-by: James Dibble <dibble-james@github.com>

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Demo;
-using BlazorStrap;
+using Havit.Blazor.Components.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddBlazorStrap();
+builder.Services.AddHxServices();
 
 await builder.Build().RunAsync();
