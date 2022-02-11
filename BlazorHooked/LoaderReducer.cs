1561131cc593ac5335f6e85ba86d94f0703daf39
James Dibble <dibble-james@github.com>
2022-02-11 22:26:37 +0000

    Add sagas and use them in the loader

--- BlazorHooked/LoaderReducer.cs
+++ BlazorHooked/LoaderReducer.cs
@@ -1,44 +1,52 @@
-﻿using System.Runtime.CompilerServices;
+﻿namespace BlazorHooked;
 
-namespace BlazorHooked;
+using System.Runtime.CompilerServices;
+using BlazorHooked.Sagas;
 
 public static class LoaderReducer
 {
-    public static (LoaderState<T>? state, Dispatch dispatch) UseLoaderReducer<T>(this HookContext context, [CallerLineNumber] int caller = 0)
-        => context.UseReducer(Reducer, new LoaderState<T>(true, default, null), caller);
+    public static (LoaderState<T>? state, Dispatch dispatch) UseLoaderReducer<T>(this HookContext context, Func<Task<T>> loader, [CallerLineNumber] int caller = 0)
+        => context.UseReducer(Reducer, new LoaderState<T>(false, false, default, null), caller).WithSaga(Saga(loader));
 
     private static LoaderState<T> Reducer<T>(LoaderState<T> state, object action) => action switch
     {
-        LoaderActions.Load => state with { Loading = true, Result = default, Failure = null },
-        LoaderActions.Loaded<T> loaded => state with { Loading = false, Result = loaded.Result },
+        LoaderActions.Load<T> => state with { Loading = true, Result = default, Failure = null, Loaded = false },
+        LoaderActions.Loaded<T> loaded => state with { Loading = false, Loaded = true, Result = loaded.Result },
         LoaderActions.Failed failed => state with { Loading = false, Failure = failed.Failure },
         _ => state
     };
 
+    private static Saga<LoaderActions.Load<T>> Saga<T>(Func<Task<T>> loader) => async (action, dispatch) =>
+    {
+        try
+        {
+            var result = await loader();
+
+            dispatch(new LoaderActions.Loaded<T>(result));
+        }
+        catch (Exception ex)
+        {
+            dispatch(new LoaderActions.Failed(ex));
+        }
+    };
 }
 
 public static class LoaderActions
 {
-    public record Load();
+    public record Load<T>();
 
     public record Loaded<T>(T Result);
 
     public record Failed(Exception Failure);
 }
 
-public record LoaderState<T>(bool Loading, T? Result, Exception? Failure)
+public record LoaderState<T>(bool Loading, bool Loaded, T? Result, Exception? Failure)
 {
     public bool TryGetResult(out T? result)
     {
-        result = default;
-
-        if (Loading || Failure is not null)
-        {
-            return false;
-        }
-
         result = Result;
-        return true;
+
+        return Loaded;
     }
 
     public bool TryGetFailure(out Exception? failure)
---
+++ BlazorHooked/SagaRegistration.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorHooked.Sagas;
+
+public delegate Task Saga<TTrigger>(TTrigger action, Dispatch dispatch);
+
+public static class SagaRegistration
+{
+    public static (T? state, Dispatch dispatch) WithSaga<T, TTrigger>(this (T? State, Dispatch Dispatch) reducer, Saga<TTrigger> saga)
+        => (reducer.State, WrapDispatch(reducer.Dispatch, saga));
+
+    private static Dispatch WrapDispatch<TTrigger>(Dispatch dispatch, Saga<TTrigger> saga) => action =>
+    {
+        dispatch(action);
+
+        if (action is TTrigger trigger)
+        {
+            Task.Run(() => saga(trigger, dispatch));
+        }
+    };
+}
